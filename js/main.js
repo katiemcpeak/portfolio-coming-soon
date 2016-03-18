867f1635867c8afb3f6dc6e55c0704b8a5c1b35b
katiemcpeak <katiemcpeak@Katies-MacBook-Pro.local>
2016-03-18 10:11:38 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-var $btnExpand = $('.btn-expand');
-var $panel = $('.panel');
-
-$btnExpand.on('click', function () {
-    $panel.toggleClass('js-panel-expand'); 
-});
-
 $('.about').localScroll();
 
 $('.portfolio').localScroll();
